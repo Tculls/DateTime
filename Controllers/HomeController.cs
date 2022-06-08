d5d10e9bfa5cab7430c2121db0eab78a263be9a5
Tculls <tanner.culley@gmail.com>
2022-06-08 16:22:37 -0700

    first commit

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Mvc;
+public class HomeController: Controller
+{
+    [HttpGet]
+    [Route("")]
+    public ViewResult Index()
+    {
+        ViewBag.CurrentTime = DateTime.Now;
+        return View("Index");
+    }
+}
